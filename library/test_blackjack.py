2bac514c4d887222bf3f217b4ac918404928ef4d
Zack Cooper <zackjcooper@gmail.com>
2015-01-21 15:46:17 -0500

    removed tests folder because I haven't a clue how to test in a different directory

--- library/test_blackjack.py
+++ library/test_blackjack.py
@@ -2,3 +2,6 @@ from class_card import Card
 from class_deck import Deck
 from class_player_hand import Player_hand
 from class_dealer_hand import Dealer_hand
+
+def test_correct_card_num():
+    assert len(Card.card_list) == 52
