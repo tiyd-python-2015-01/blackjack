b1c75c34aad71e6bafbf1a43dc74c5e1d6fe9615
Daniel Newell <dknwell1@gmail.com>
2015-01-21 16:15:14 -0500

    Got initial Deck and Card class working, as well as some tests

--- deck.py
+++ deck.py
@@ -1,31 +1,39 @@
 import random
 from card import Card
-my_deck = []
 
 class Deck:
-    card_suits = ["Clubs", "Hearts", "Spades", "Diamonds"]
-    card_numbers = ["2","3","4","5","6","7","8","9","10", "ACE",
-                    "KING","QUEEN","JACK"]
-## self.cards[0..52] = Card(number,suit) deck = Deck(...)    deck = Deck(...)
-#deck.cards[]
 
+    def __init__(self):
+        self.my_deck = []
 
-
-    def generate_cards():
         card_suits = ["Clubs", "Hearts", "Spades", "Diamonds"]
-        card_numbers = ["2","3","4","5","6","7","8","9","10", "ACE",
-                        "KING","QUEEN","JACK"]
+        card_numbers = ["2","3","4","5","6","7","8","9","10", "Ace",
+                        "King","Queen","Jack"]
         for suit in card_suits:
             for number in card_numbers:
-                if number in ["KING","QUEEN","JACK"]:
+                if number in ["King","Queen","Jack"]:
                     value = 10
-                elif number == "ACE":
+                elif number == "Ace":
                     value = 1,11
                 else:
                     value = int(number)
                 card = Card(number, suit,value)
-                my_deck.append(card)
+                self.my_deck.append(card)
 
-Deck.generate_cards()
+    def get_length(self):
+        return len(self.my_deck)
 
-print(len(my_deck))
+    def __str__(self):
+        return str(self.my_deck)
+
+    def get_card(self):
+        rand_card = self.my_deck.pop(random.randint(0,len(self.my_deck)-1))
+        print(rand_card)
+        return rand_card
+"""
+new_deck = Deck()
+print(new_deck.get_length())
+new_deck.get_card()
+print(new_deck.get_length())
+
+"""
