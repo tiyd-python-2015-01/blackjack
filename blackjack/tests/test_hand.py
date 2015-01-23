4f4e1104a28eee7d8f5209ae3045319ead9e40c1
Gordon Fierce <gordon.fierce@gmail.com>
2015-01-23 13:01:45 -0500

    Tested can_split function.

--- blackjack/tests/test_hand.py
+++ blackjack/tests/test_hand.py
@@ -2,6 +2,11 @@ from hand import Hand
 from card import Card
 from deck import Deck
 
+spade_of_eights = Card("Spades", "8")
+heart_of_eights = Card("Hearts", "8")
+a_card = Card("Spades", "4")
+a_second_card = Card("Hearts", "5")
+
 
 def test_hand_grabs():
     my_hand = Hand()
@@ -11,8 +16,6 @@ def test_hand_grabs():
 
 
 def test_hand_prints():
-    spade_of_eights = Card("Spades", "8")
-    heart_of_eights = Card("Hearts", "8")
     my_hand = Hand()
     my_hand.grab(spade_of_eights)
     my_hand.grab(heart_of_eights)
@@ -23,8 +26,15 @@ def test_hand_prints():
 
 def test_hand_value():
     my_hand = Hand()
-    a_card = Card("Spades", "4")
     my_hand.grab(a_card)
-    a_second_card = Card("Hearts", "5")
     my_hand.grab(a_second_card)
     assert my_hand.value() == 9
+
+
+def test_hand_is_splittable():
+    can_split_hand = Hand(spade_of_eights, heart_of_eights)
+    can_not_split_hand = Hand(a_card, a_second_card)
+    too_big_to_split_hand = Hand(a_card, a_second_card, spade_of_eights)
+    assert can_split_hand.can_split()
+    assert not can_not_split_hand.can_split()
+    assert not too_big_to_split_hand.can_split()
