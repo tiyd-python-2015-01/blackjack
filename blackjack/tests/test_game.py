9de4bf2d3b46b7ce224ebe869b2baf3153096900
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-23 11:48:16 -0500

    pep8

---
+++ blackjack/tests/test_game.py
@@ -0,0 +1,20 @@
+from card import *
+from hand import *
+from player import *
+from card import *
+from game import *
+
+mygame = Game()
+
+
+def test_init():
+    assert mygame.winner == ""
+
+
+def test_set_winner():
+    mygame.set_winner("D")
+    assert mygame.winner == "D"
+
+
+def test_get_winner():
+    assert mygame.get_winner() == "D"
