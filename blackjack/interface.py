32f5eeecc0cae72ca10c07419811ebb6d2d73792
Alan Grissett <alan.grissett@gmail.com>
2015-01-23 19:04:50 -0500

    Added to options menu to the interface file, also added a method to modify the gameoptions object to reflect selections made in the options menu.

--- blackjack/interface.py
+++ blackjack/interface.py
@@ -1,3 +1,6 @@
+from game_options import GameOptions
+
+
 class Interface:
     """ Interface class will provide all of the methods of interacting with
     the user.  It will contain all of the methods for displaying to the
@@ -7,24 +10,23 @@ class Interface:
     * Outputting game information and menus to the player
     * Receiving input from the player
     """
-    
 
     def main_menu(self):
         """ Displays the title, programmer info, and main menu for the game.
         Takes user input and returns to the calling Game class"""
         title_text = ("\n"
-        ".------..------..------..------..------."
-        ".------..------..------..------.\n"
-        "|B.--. ||L.--. ||A.--. ||C.--. ||K.--. |"
-        "|J.--. ||A.--. ||C.--. ||K.--. |\n"
-        "| :(): || :/\: || (\/) || :/\: || :/\: |"
-        "| :(): || (\/) || :/\: || :/\: |\n"
-        "| ()() || (__) || :\/: || :\/: || :\/: |"
-        "| ()() || :\/: || :\/: || :\/: |\n"
-        "| '--'B|| '--'L|| '--'A|| '--'C|| '--'K|"
-        "| '--'J|| '--'A|| '--'C|| '--'K|\n"
-        "`------'`------'`------'`------'`------'"
-        "`------'`------'`------'`------'")
+                      ".------..------..------..------..------."
+                      ".------..------..------..------.\n"
+                      "|B.--. ||L.--. ||A.--. ||C.--. ||K.--. |"
+                      "|J.--. ||A.--. ||C.--. ||K.--. |\n"
+                      "| :(): || :/\: || (\/) || :/\: || :/\: |"
+                      "| :(): || (\/) || :/\: || :/\: |\n"
+                      "| ()() || (__) || :\/: || :\/: || :\/: |"
+                      "| ()() || :\/: || :\/: || :\/: |\n"
+                      "| '--'B|| '--'L|| '--'A|| '--'C|| '--'K|"
+                      "| '--'J|| '--'A|| '--'C|| '--'K|\n"
+                      "`------'`------'`------'`------'`------'"
+                      "`------'`------'`------'`------'")
 
         info = ("Programmed by: Alan Grissett\n"
                 "The Iron Yard - Durham\n"
@@ -45,3 +47,103 @@ class Interface:
         selection = input("♤ ")
 
         return selection
+
+    def get_name(self):
+        """Prompts the user to input a name, used for the creation of a
+        Player object."""
+        print("\n"*80)
+        print("Great!  Let's play some Blackjack!")
+        name = input("Please enter your name: ")
+        return name
+
+    def options_menu(self, options):
+        """Displays the options menu and updates option variables to reflect
+        user selections."""
+        while True:
+            print("\n"*80)
+            print("Game Options:\n")
+            print("1 - Number of Decks: {}".format(options.number_of_decks))
+            print("2 - Dealer Hits on Soft 17: {}".format("Enabled" if
+                                                          options.hit_soft_17
+                                                          else "Disabled"))
+            print("3 - Early Surrender: {}".format("Enabled" if
+                                                   options.early_surrender
+                                                   else "Disabled"))
+            print("4 - No Surrender: {}".format("Enabled" if
+                                                options.no_surrender
+                                                else "Disabled"))
+            print("5 - Resplitting Allowed: {}".format("Enabled" if
+                                                       options.resplitting
+                                                       else "Disabled"))
+            print("6 - Resplit Aces: {}".format("Enabled" if
+                                                options.resplit_aces
+                                                else "Disabled"))
+            print("7 - Hit Split Aces: {}".format("Enabled" if
+                                                  options.hit_split_aces
+                                                  else "Disabled"))
+            print("8 - Split By Rank/Value: {}".format("Rank" if
+                                                       options.split_by_rank
+                                                       else "Value"))
+            print("9 - No Double After Split: ", end="")
+            print("{}".format("Enabled" if options.no_double_after_split
+                              else "Disabled"))
+            print("10 - Double on 9-10-11 ", end="")
+            print("Only: {}".format("Enabled" if options.double_9_10_11
+                                    else "Disabled"))
+            print("11 - Double on 9-10 Only: {}".format("Enabled" if
+                                                        options.double_9_10
+                                                        else "Disabled"))
+            print("R - Reset to Defaults")
+            print("Q - Return to Main Menu")
+            selection = input("♡ ")
+
+            if selection.upper() == "Q":
+                return options
+            else:
+                options = self.options_menu_input(selection, options)
+
+    def options_menu_input(self, selection, options):
+        """ This method receives a selection from the options menu
+        and updates the game options object to reflect the desired
+        ruleset selected by the player."""
+        if selection == "1":
+            try:
+                decks = int(input("Choose number of decks (8 max):  "))
+                if decks < 1 or decks > 8:
+                    raise ValueError
+                options.number_of_decks = decks
+            except ValueError:
+                input("Invalid input.  Press enter to continue.")
+        elif selection == "2":
+            options.hit_soft_17 = not options.hit_soft_17
+        elif selection == "3":
+            options.early_surrender = not options.early_surrender
+            if options.no_surrender:
+                options.no_surrender = False
+        elif selection == "4":
+            options.no_surrender = not options.no_surrender
+            if options.early_surrender:
+                options.early_surrender = False
+        elif selection == "5":
+            options.resplitting = not options.resplitting
+        elif selection == "6":
+            options.resplit_aces = not options.resplit_aces
+        elif selection == "7":
+            options.hit_split_aces = not options.hit_split_aces
+        elif selection == "8":
+            options.split_by_rank = not options.split_by_rank
+        elif selection == "9":
+            options.no_double_after_split = (not
+                                             options.no_double_after_split)
+        elif selection == "10":
+            options.double_9_10_11 = not options.double_9_10_11
+            if options.double_9_10:
+                options.double_9_10 = False
+        elif selection == "11":
+            options.double_9_10 = not options.double_9_10
+            if options.double_9_10_11:
+                options.double_9_10_11 = False
+        elif selection.upper() == "R":
+            options = GameOptions()
+
+        return options
