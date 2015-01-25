2ad1ba797c1b7feab70b50ee9e6f192d82c7f538
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-25 12:07:51 -0500

    final tests for Card class

--- blackjack/tests/test_card.py
+++ blackjack/tests/test_card.py
@@ -1,8 +1,8 @@
 from card import *
 
 mycard = Card("J", "Diamonds")
-mycard1 = Card("A", "Diamonds")
-mycard2 = Card("8", "Diamonds")
+mycard1 = Card("A", "Clubs")
+mycard2 = Card("8", "Hearts")
 
 
 def test_init():
@@ -14,3 +14,15 @@ def test_getvalue():
     assert mycard.get_value() == 10
     assert mycard1.get_value() == 1
     assert mycard2.get_value() == 8
+
+
+def test_get_rank():
+    assert mycard.get_rank() == "J"
+    assert mycard1.get_rank() == "A"
+    assert mycard2.get_rank() == "8"
+
+
+def test_get_suit():
+    assert mycard.get_suit() == "Diamonds"
+    assert mycard1.get_suit() == "Clubs"
+    assert mycard2.get_suit() == "Hearts"
