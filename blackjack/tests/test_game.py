5a34b4c11b3d2dfd6da540c2ca3c4e2f3cf5f430
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-25 11:56:25 -0500

    final tests for Game class

--- blackjack/tests/test_game.py
+++ blackjack/tests/test_game.py
@@ -4,17 +4,21 @@ from player import *
 from card import *
 from game import *
 
+
 mygame = Game()
 
 
 def test_init():
-    assert mygame.winner == ""
+    assert mygame.winners == []
 
 
 def test_set_winner():
     mygame.set_winner("D")
-    assert mygame.winner == "D"
+    assert mygame.winners == ["D"]
+    mygame.set_winner("P")
+    assert mygame.winners == ["D", "P"]
 
 
 def test_get_winner():
-    assert mygame.get_winner() == "D"
+    mygame.set_winner("T")
+    assert mygame.get_winners() == ["D", "P", "T"]
