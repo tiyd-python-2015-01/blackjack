0d88baa6d245b106bd7e81c4d002e6ea24a97d19
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-25 15:57:02 -0500

    Final Dealer Class

--- blackjack/dealer.py
+++ blackjack/dealer.py
@@ -1,6 +1,7 @@
 from deck import *
 from hand import *
 
+
 class Dealer:
     """A dealer for the game.
 
@@ -10,9 +11,18 @@ class Dealer:
         * Has a budget
         * Has a status: Bust, Hit, Stand
         * Methods:
-            Gets_budget()
-            Gets_status()
-
+            Deal() - gives ne card
+            add_card() adds a card to its hand
+            Get_hand()) returns the cards in the hand
+            get_hand_value() returns the value of the hand
+            get_hand_status() the hand status is hard or soft
+                (1 A with value of 11)
+            set_hand_status() changes the value of the hand from
+                hard to soft and viceversa
+            Get_status() Status is Hit when dealer still getting cards.
+                Bust when dealer goes over 21
+            Set_status: sets to B when dealer busts, set to T when there is
+                a tie (push)
         Collaborators:
 
         * Has a hand of cards
@@ -28,11 +38,22 @@ class Dealer:
     def deal(self):
         return self.__deck__.deal()
 
-    def add_card(self,card):
+    def add_card(self, card):
         self.__hand__.add_card(card)
 
+    def get_hand(self):
+        return self.__hand__
 
-    def updates_status(self, newstatus):
+    def get_hand_value(self):
+        return self.__hand__.get_value()
+
+    def get_hand_status(self):
+        return self.__hand__.get_hand_status()
+
+    def set_hand_status(self, status):
+        return self.__hand__.set_hand_status(status)
+
+    def set_status(self, newstatus):
         self.__status__ = newstatus
 
     def get_status(self):
