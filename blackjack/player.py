0f11159d2281be28733a9f279707491fc493d77b
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-25 16:07:49 -0500

    final player class

--- blackjack/player.py
+++ blackjack/player.py
@@ -1,5 +1,6 @@
 from hand import *
 
+
 class Player:
     """A player.
 
@@ -18,6 +19,21 @@ class Player:
 
     * Has a hand of cards
     * Receives cards from the deck
+    * Methods:
+    add_card() adds a card to its hand
+    Get_hand()) returns the cards in the hand
+    get_hand_value() returns the value of the hand
+    get_hand_status() the hand status is hard or soft
+        (1 A with value of 11)
+    set_hand_status() changes the value of the hand from
+        hard to soft and viceversa
+    Get_status() Status is Hit when dealer still getting cards.
+        Bust when dealer goes over 21
+    Set_status: sets to B when dealer busts, set to T when there is
+        a tie (push)
+    Get_budget
+    Set_budget
+
     """
 
     def __init__(self, name, budget):
@@ -26,20 +42,32 @@ class Player:
         self.__budget__ = budget
         self.__hand__ = Hand()
 
-    def add_card(self,card):
+    def add_card(self, card):
         self.__hand__.add_card(card)
 
-    def updates_budget(self, change):
+    def get_hand_value(self):
+        return self.__hand__.get_value()
+
+    def get_hand_status(self):
+        return self.__hand__.get_hand_status()
+
+    def set_hand_status(self, status):
+        return self.__hand__.set_hand_status(status)
+
+    def set_budget(self, change):
         self.__budget__ += change
 
     def get_budget(self):
         return self.__budget__
 
-    def updates_status(self, newstatus):
+    def set_status(self, newstatus):
         self.__status__ = newstatus
 
     def get_status(self):
         return self.__status__
+
+    def get_hand(self):
+        return self.__hand__
 
     def __repr__(self):
         return "{}".format(self.__name__)
