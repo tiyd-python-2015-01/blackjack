57e24bd991dcc543979deecdf62af70ee846bb3a
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-25 15:57:26 -0500

    final tests dealer class

--- blackjack/tests/test_dealer.py
+++ blackjack/tests/test_dealer.py
@@ -7,8 +7,8 @@ from dealer import *
 mydealer = Dealer()
 
 mycard1 = Card("A", "Diamonds")
-mycard2 = Card("8", "Diamonds")
-mycard3 = Card("9", "Hearts")
+mycard2 = Card("2", "Diamonds")
+mycard3 = Card("3", "Hearts")
 myhand = Hand()
 
 
@@ -17,20 +17,38 @@ def test_init():
     assert type(mydealer.__hand__) == Hand
     assert type(mydealer.__deck__) == Deck
 
+
 def test_dealer_deals():
     assert type(mydealer.deal()) == Card
     assert mydealer.__deck__.get_length() == 51
 
+
 def test_dealer_add_card():
+    assert mydealer.add_card(mycard1) == ['A of Diamonds']
+    assert mydealer.add_card(mycard2) == ['A of Diamonds', '2 of Diamonds']
+    assert mydealer.add_card(mycard3) == ['A of Diamonds', '8 of Diamonds',
+                                          '9 of Hearts']
 
-    mydealer.add_card(mycard3)
 
-def test_get_status():
-    assert mydealer.get_status() == ''
-
-
-def test_set_status():
-    mydealer.updates_status('H')
+def test_set_and_get_status():
+    mydealer.set_status('H')
     assert mydealer.get_status() == 'H'
-    mydealer.updates_status('S')
-    assert mydealer.get_status() == 'S'
+    mydealer.set_status('B')
+    assert mydealer.get_status() == 'B'
+
+
+def test_get_hand():
+    assert mydealer.get_hand() == ['A of Diamonds', '8 of Diamonds',
+                                   '9 of Hearts']
+
+
+def test_and_get_hand_status():
+    mydealer.set_hand_status('S')
+    assert mydealer.get_hand_status() == 'S'
+
+
+def get_hand_value():
+    mydealer.set_hand_status('S')
+    assert mydealer.get_hand_value() == 16
+    mydealer.set_hand_status('H')
+    assert mydealer.get_hand_value() == 6
