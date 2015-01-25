cc36485e2ebc5bfdd6ad8be4cab0cd76b1bd9f7e
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-25 11:55:45 -0500

    Final Game Class

--- blackjack/game.py
+++ blackjack/game.py
@@ -1,22 +1,23 @@
 class Game:
     """Stores Information about a Game
     Responsibilities:
-    * Keeps information about who won the game: Dealer, Player, Push
-    * Methods: gets_winner() to get information on who won the game
+    * Keeps information in a list of the winners of each game: Dealer, Player, Push
+    * gets_winner() to get information on who won the game
+    * set_winner() to update list with new winner
     Collaborators:
     * Dealer, Player"""
 
     def __init__(self):
-        self.winner = ''
+        self.winners = []
 
     def set_winner(self, whowon):
-        self.winner = whowon
+        self.winners.append(whowon)
 
-    def get_winner(self):
-        return self.winner
+    def get_winners(self):
+        return self.winners
 
     def __repr__(self):
-        return "{}".format(self.winner)
+        return "{}".format(self.winners)
 
     def __str__(self):
-        return "{}".format(self.winner)
+        return "{}".format(self.winners)
