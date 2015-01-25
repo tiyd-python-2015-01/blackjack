267fc53078eb93d2fa56bb1eec59be3b205b477a
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-25 12:54:23 -0500

    final tests hand class

--- blackjack/tests/test_hand.py
+++ blackjack/tests/test_hand.py
@@ -7,6 +7,7 @@ mycard2 = Card("8", "Diamonds")
 mycard3 = Card("9", "Hearts")
 
 myhand = Hand()
+
 myhand.add_card(mycard1)
 myhand.add_card(mycard2)
 
@@ -21,15 +22,17 @@ def test_get_status():
 
 
 def test_set_status():
-    myhand.update_hand_status("S")
+    myhand.set_hand_status("S")
     assert myhand.get_hand_status() == "S"
-    myhand.update_hand_status("H")
+    myhand.set_hand_status("H")
     assert myhand.get_hand_status() == "H"
+
 
 def test_get_hand_value():
     assert myhand.get_value() == 9
-    myhand.update_hand_status("S")
+    myhand.set_hand_status("S")
     assert myhand.get_value() == 19
+
 
 def test_hand_get_length():
     assert len(myhand) == 2
